bf778b5708a7986bbd08bbb586879db9f17f1ef8
Patryk Karwowski <p-karwowski-dev@gmail.com>
2026-04-06 19:41:31 +0100

    fixed nav style names

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,20 +2,23 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { activeTab, inactiveTab } from "./style";
+import { activeTabClass, inactiveTabClass } from "./style";
 
 export default function Nav() {
   const pathname = usePathname();
 
   return (
     <nav className="w-full bg-gray-800 flex items-center justify-center gap-0 py-2">
-      <Link href="/" className={pathname === "/" ? activeTab : inactiveTab}>
+      <Link
+        href="/"
+        className={pathname === "/" ? activeTabClass : inactiveTabClass}
+      >
         Home
       </Link>
       <span className="border-l border-gray-400 h-6" />
       <Link
         href="/add-user"
-        className={pathname === "/add-user" ? activeTab : inactiveTab}
+        className={pathname === "/add-user" ? activeTabClass : inactiveTabClass}
       >
         User Form
       </Link>
--- src/components/nav/style.ts
+++ src/components/nav/style.ts
@@ -1,2 +1,2 @@
-export const activeTab = "px-6 text-gray-100";
-export const inactiveTab = "px-6 text-gray-400 hover:text-gray-100";
+export const activeTabClass = "px-6 text-gray-100";
+export const inactiveTabClass = "px-6 text-gray-400 hover:text-gray-100";
